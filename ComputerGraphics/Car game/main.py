76c1b23ce70a8d91aebdfd1a76083544f6e4f9b1
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-04-29 18:37:28 -0300

    objects organized

--- ComputerGraphics/Car game/main.py
+++ ComputerGraphics/Car game/main.py
@@ -6,61 +6,61 @@ import numpy as np
 import glfw
 from OpenGL.GL import *
 
-def scale_move_rot(s = 1, x = 0, y = 0, angle = 0) : 
+def scale_move_rot(s = 100, x = 0, y = 0, angle = 0) : 
     mat_transform = trans.createEyeMat()
     mat_transform = trans.scale(s/100, s/100, mat_transform)
     mat_transform = trans.translate(x/100, y/100, mat_transform)
     mat_transform = trans.rotateZ(angle, mat_transform)
     return mat_transform
 
-def car_object( ) :
-    global loc_color
-    global loc
-    global car, tire
-
-    # BODY
-    mat_transform = scale_move_rot( s = 20 )
-    # Translate with keyboard
-    mat_transform = trans.translate(tx, ty, mat_transform)
+def car_object( color = (0,0,0), S = 100, X = 0, Y = 0) :
+    ## BODY
+    comp_transform = scale_move_rot( s = 20 )
+    obj_transform = scale_move_rot( s = S, x = 100*X, y = 100*Y )    # Translate with keyboard
+    mat_transform = np.dot( obj_transform, comp_transform )
+    # matrix = np.array([[-1.0, 0.0, 0.0, 0],
+    #                             [0.0, 1.0, 0.0, 0],
+    #                             [0.0, 0.0, 1.0, 0.0],
+    #                             [0.0, 0.0, 0.0, 1.0]], np.float32)
+    # mat_transform = np.dot(matrix, mat_transform)
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
-    R, G, B = 1, .1, .1
+    R, G, B = color[0], color[1], color[2]
     glUniform4f(loc_color, R, G, B, 1.0)
     glDrawArrays(GL_TRIANGLE_FAN, 0, len(car))
     ## RIGHT WHEEL
     # OUTSIDE
-    mat_transform = scale_move_rot( s = 10, x = 22, y = 8, angle = theta )
-    # Translate with keyboard
-    mat_transform = trans.translate(tx, ty, mat_transform)
+    comp_transform = scale_move_rot( s = 10, x = 22, y = 8, angle = theta )
+    obj_transform = scale_move_rot( s = S, x = 100*X, y = 100*Y )    # Translate with keyboard
+    mat_transform = np.dot( obj_transform, comp_transform )
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
     R, G, B = .1, .1, .1
     glUniform4f(loc_color, R, G, B, 1.0)
     glDrawArrays(GL_TRIANGLE_FAN, len(car), len(tire))
     # INSIDE
-    mat_transform = scale_move_rot( s = 5, x = 22, y = 8, angle = theta )
-    # Translate with keyboard
-    mat_transform = trans.translate(tx, ty, mat_transform)
+    comp_transform = scale_move_rot( s = 5, x = 22, y = 8, angle = theta )
+    obj_transform = scale_move_rot( s = S, x = 100*X, y = 100*Y )    # Translate with keyboard
+    mat_transform = np.dot( obj_transform, comp_transform )
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
     R, G, B = .2, .2, .2
     glUniform4f(loc_color, R, G, B, 1.0)
     glDrawArrays(GL_TRIANGLE_FAN, len(car), len(tire))
     ## LEFT WHEEL
     # OUTSIDE
-    mat_transform = scale_move_rot( s = 10, x = -22, y = 8, angle = theta )
-    # Translate with keyboard
-    mat_transform = trans.translate(tx, ty, mat_transform)
+    comp_transform = scale_move_rot( s = 10, x = -22, y = 8, angle = theta )
+    obj_transform = scale_move_rot( s = S, x = 100*X, y = 100*Y )    # Translate with keyboard
+    mat_transform = np.dot( obj_transform, comp_transform )
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
     R, G, B = .1, .1, .1
     glUniform4f(loc_color, R, G, B, 1.0)
     glDrawArrays(GL_TRIANGLE_FAN, len(car), len(tire))
     # INSIDE
-    mat_transform = scale_move_rot( s = 5, x = -22, y = 8, angle = theta )
-    # Translate with keyboard
-    mat_transform = trans.translate(tx, ty, mat_transform)
+    comp_transform = scale_move_rot( s = 5, x = -22, y = 8, angle = theta )
+    obj_transform = scale_move_rot( s = S, x = 100*X, y = 100*Y )    # Translate with keyboard
+    mat_transform = np.dot( obj_transform, comp_transform )
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
     R, G, B = .2, .2, .2
     glUniform4f(loc_color, R, G, B, 1.0)
     glDrawArrays(GL_TRIANGLE_FAN, len(car), len(tire))
-
 
 # Create the window with the given parameters
 window = gh.createWindow(800, 800, "Move: WASD or ArrowKeys, Rotate: to left Q and to right E")
@@ -124,8 +124,9 @@ while not glfw.window_should_close(window):
     gh.dt_y = 0
     gh.dtheta = 0
 
-    car_object()
-
+    # Sets the controlable car
+    car_object( color = (.2, 0, 1), S = 100, X = tx, Y = ty )
+    car_object( S=150, X=tx, Y=ty-.8 )
 
     glfw.swap_buffers(window)
 
